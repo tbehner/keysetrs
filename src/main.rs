6d1b1f45fa4be6f25709c7e1696d27a28ba64dea
Timm Behner <behner@cs.uni-bonn.de>
2017-09-01 15:43:32 +0200

    Intermediate step

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 extern crate libusb;
 use std::thread;
 use std::sync::mpsc;
+use std::collections::HashMap;
+use std::process::Command;
+use std::str;
+
+#[derive(Debug)]
+struct KeyboardConfig {
+    name: String,
+    layout: String,
+    variant: String,
+}
 
 fn produce_ids(tx: mpsc::Sender<String>) {
     let usbctx = libusb::Context::new().unwrap();
@@ -10,14 +20,39 @@ fn produce_ids(tx: mpsc::Sender<String>) {
     }
 }
 
+fn set_keyboard(keyboard_identifier: &str) {
+    let output = Command::new("echo")
+        .arg("Hello World")
+        .output()
+        .expect("Failed to execute process!");
+    println!("{}", str::from_utf8(&output.stdout).unwrap());
+}
+
 fn main() {
-    let matias_ergo_pro_id = String::from("05e3:0614");
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || {produce_ids(tx)});
 
+    let mut keyboard_configs: HashMap<String, KeyboardConfig> = HashMap::new();
+    let mut keyboard_ids: HashMap<String, String> = HashMap::new();
+
+    keyboard_configs.insert("Matias Ergo Pro".to_string(), 
+                            KeyboardConfig{
+                                name    : "Matias Ergo Pro".to_string(),
+                                layout  : "us".to_string(),
+                                variant : "alt-intl".to_string(),
+                            }
+                            );
+
+    keyboard_ids.insert("05e3:0614".to_string(), "Matias Ergo Pro".to_string());
+
     for id in rx {
-        if id == matias_ergo_pro_id {
-            println!("Found matias ergo pro: {}", id);
+        match keyboard_ids.get(&id) {
+            Some(name) => {
+                println!("Found {}", name);
+                println!("Using {:?}", keyboard_configs[name]);
+                set_keyboard(name);
+            },
+            None => continue,
         }
     }
 }
