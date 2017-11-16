d50e45c4020040e676f58284231d857f0e067aba
Timm Behner <behner@cs.uni-bonn.de>
2017-11-16 14:53:25 +0100

    Finds keyboards as ID and cofig
    Also checks for errors which I didn't even think of in the go
    implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 #![recursion_limit = "1024"]
 
-#[macro_use]
-extern crate error_chain;
+#[macro_use] extern crate error_chain;
+mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain! { 
+    
+        foreign_links {
+            Libusb(::libusb::Error);
+        }
+    }
+}
+use errors::*;
 
-#[macro_use]
-extern crate serde_derive;
 
+#[macro_use] extern crate serde_derive;
 extern crate serde;
 extern crate toml;
+extern crate structopt;
+#[macro_use] extern crate structopt_derive;
 
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
+use structopt::StructOpt;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Config {
     keyboards: HashMap<String, Keyboard>,
     usbids: HashMap<String, String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Keyboard {
     mapping: String,
     variant: String,
@@ -27,41 +38,55 @@ struct Keyboard {
     xmodmapconfig: String,
 }
 
-mod errors {
-    // Create the Error, ErrorKind, ResultExt, and Result types
-    error_chain! { }
+impl Keyboard {
+    fn xkbmap_command_args(&self) -> String {
+        return format!("{} -variant {}", self.mapping, self.variant)
+    }
 }
-use errors::*;
 
+#[derive(StructOpt, Debug)]
+struct Cli {
+    #[structopt(short = "d", long = "default", default_value = "104", help="Set the default keyboard.")]
+    default: String,
 
+    #[structopt(long = "debug", help="Don't execute, just show the commands.")]
+    debug: bool,
 
+    #[structopt(short = "l", long = "list", help="Show all keyboard configurations.")]
+    list: bool,
+
+    #[structopt(short = "a", long = "attached", help="Show all attached keyboards.")]
+    attached: bool,
+}
+    
 extern crate libusb;
-// use std::thread;
-// use std::sync::mpsc;
-// use std::process::Command;
+use std::process::Command;
 use std::str;
 
-// fn produce_ids(tx: mpsc::Sender<String>) {
-//     let usbctx = libusb::Context::new().unwrap();
-//     for device in usbctx.devices().unwrap().iter() {
-//         let dev_desc = device.device_descriptor().unwrap();
-//         tx.send(format!("{:04x}:{:04x}", dev_desc.vendor_id(), dev_desc.product_id()));
-//     }
-// }
 
-// fn set_keyboard(keyboard_identifier: &str) {
-//     let output = Command::new("echo")
-//         .arg("Hello World")
-//         .output()
-//         .expect("Failed to execute process!");
-//     println!("{}", str::from_utf8(&output.stdout).unwrap());
-// }
+fn get_usb_ids() -> Result<Vec<String>> {
+    let usbctx = libusb::Context::new()?;
+    let mut result: Vec<String> = Vec::new();
+    for device in usbctx.devices()?.iter() {
+        let dev_desc = device.device_descriptor()?;
+        result.push(format!("{:04x}:{:04x}", dev_desc.vendor_id(), dev_desc.product_id()));
+    }
+    Ok(result)
+}
 
-quick_main!(run);
+fn set_keyboard(keyboard: &Keyboard) {
+    let output = Command::new("echo")
+        .arg(keyboard.xkbmap_command_args())
+        .output()
+        .expect("Failed to execute process!");
+    println!("{}", str::from_utf8(&output.stdout).unwrap());
+}
+
 
 fn open(path: &str) -> Result<File> {
     File::open(path).chain_err(|| format!("Can't open `{}`", path))
 }
+
 
 fn read_file(path: &str) -> Result<String> {
     let mut result = String::new();
@@ -70,17 +95,54 @@ fn read_file(path: &str) -> Result<String> {
     Ok(result)
 }
 
+
+quick_main!(run);
+
+
 fn run() -> Result<()> {
+    let cli = Cli::from_args();
     // TODO this should not be hard coded, but read from ENV
     let config_content = read_file("/home/timm.behner/dotfiles/keyset.toml")?;
     let config: Config = toml::from_str(config_content.as_str()).chain_err(|| "Unable to parse toml config")?;
-    for (k, v) in config.keyboards.iter() {
-        println!("{} : {:?}", k, v.mapping);
+
+    println!("Config {:?}", cli);
+
+    if cli.list {
+        println!("All configured keyboards:");
+        for (k, _) in config.keyboards.iter() {
+            println!("{}", k);
+        }
+        return Ok(())
     }
 
-    for (id, conf) in config.usbids.iter() {
-        println!("{} should be configured as {}", id, conf);
+    let usbids = get_usb_ids()?;
+    if cli.attached {
+        for usb_id in usbids.iter() {
+            match config.usbids.get(usb_id){
+                Some(kb) => println!("{} => {}", usb_id, kb),
+                None => {} ,
+            }
+        }
+        return Ok(())
     }
+
+    if cli.debug {
+        println!("Command: ");
+        return Ok(())
+    } else {
+        for id in usbids.iter() {
+            match config.usbids.get(id) {
+                Some(kb_name) => {
+                    match config.keyboards.get(kb_name) {
+                        Some(kb) => set_keyboard(kb),
+                        None => return Err(format!("{} is missing a configuration", kb_name).into()),
+                    }
+                },
+                None => {}
+            }
+        }
+    }
+
     Ok(())
 }
 
